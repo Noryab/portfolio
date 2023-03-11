6f0f6a7c70776a5add9a22995b96f03531ad597b
noryab <balgaci@yahoo.es>
2023-03-11 15:11:32 +0000

    add: work- workitems components

---
+++ src/components/Work.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Workitem from './Workitem'
+
+
+const data = [{
+    year:2023,
+    title:"title",
+    duration:1,
+    description:""
+}]
+
+
+const Work = () => {   
+  return (
+    <div id='work'>Work</div>
+  )
+}
+Workitem
+
+export default Work
