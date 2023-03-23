e2ed575fd0ae62f380e371602e3801551ee9e2e6
noryab <balgaci@yahoo.es>
2023-03-23 17:11:46 +0000

    add: text to work and profile img

--- next.config.js
+++ next.config.js
@@ -5,10 +5,17 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'www.creative-tim.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
       },
+
     ],
     // domains: ['images.unsplash.com'],
   },
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
 import { FaTwitter, FaGithub, FaLinkedinIn} from "react-icons/fa";
-import OneImg from "public/assets/one.jpg";
+import ProfileImg from "public/assets/profile2.JPG";
 import Image from "next/image";
 
 const Main = () => {
   return (
     <div id="main">
-      <Image
-        className="bg-cover w-full h-screen object-left"
-        src="https://images.unsplash.com/photo-1464746133101-a2c3f88e0dd9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=843&q=80"
-        // src={OneImg}
-        alt="cover image"
-        width={200}
-        height={400}
-        responsive
-      />
-      <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
+      
+      <div className="flex flex-wrap  justify-center  items-center p-5 sm:justify-center sm:pt-9 lg:items-center sm:flex-row text-justify relative">
+        <div className="rounded-full basis-[50%] ">
+            <Image                
+            className="shadow-lg mx-auto rounded-3xl h-40 w-44 align-middle border-solid border-1 shadow-slate-600"
+            // src="https://images.unsplash.com/photo-1464746133101-a2c3f88e0dd9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=843&q=80"
+            src={ProfileImg}
+            alt="cover image"
+            width={400}
+            height={200}            
+            />
+          </div>
+      </div>
+      <div className="w-ful top-0 left-0 bg-white/30">
         <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
           <h1 className="flex text-center sm:text-4xl text-3xl font-bold text-gray-800">
             I am Alejandro Garzon
