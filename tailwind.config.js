26002624eadf8bef41a98c4769da1722e9d04e60
noryab <balgaci@yahoo.es>
2023-03-24 07:56:11 +0000

    add: to url image''

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {  
 
-  content: ["./index.html",
+  content: ["./index.html",            
             "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -15,7 +15,7 @@ module.exports = {
         resume: "wiggle 200ms ease-in-out"
       },
       backgroundImage: {
-        bg2: "url('/assets/bg2.jpg')",
+        bg2: "url('./public/assets/bg2.jpg')",
       },
 
     },
