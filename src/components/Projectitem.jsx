2b6473a9b15fb6685c23894277b195785bb9677b
noryab <balgaci@yahoo.es>
2023-03-28 19:13:50 +0000

    add: cards to skills and projects

--- src/components/Projectitem.jsx
+++ src/components/Projectitem.jsx
@@ -5,24 +5,52 @@ import Link from "next/link";
 const Projectitem = ({ img, title, description }) => {
   // img ='https://images.unsplash.com/photo-1464746133101-a2c3f88e0dd9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=843&q=80'
   return (
-    <div className="relative flex  justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
+    <Link
+      href="#"
+      className="relative flex justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r text-blue-600 hover:text-blue-200"
+    >
       <Image
         src={img}
         alt="/"
-        className="rounded-xl group-hover:brightness-50"
+        className="block rounded-xl group-hover:brightness-50 group-hover:scale-105 group-hover:z-10 group-hover:overflow-hidden"
       />
-      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-        <h3 className="text-2xl font-bold text-white tracking-wider text-center">
+      <Link href="#">
+        <h5 className="group-hover:z-10 group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-100%] group-hover:translate-y-[-400%] text-center mb-2 text-2xl font-bold tracking-tight  hover:text-blue-200 dark:text-white transition duration-300">
           {title}
-        </h3>
-        <p className="pb-4 pt-2 text-white text-center">{description}</p>
-        <Link href="/">
-          <p className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg">
-            More info
+        </h5>
+      </Link>
+
+      <div className="group-hover:block group-hover:z-10 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[50%] group-hover:translate-y-0 transition duration-500 ">
+        <div className="hidden  group-hover:block">
+          <p className="mb-3 font text-xl text-white dark:text-gray-400 ">
+            {description} Here are the biggest enterprise technology
+            acquisitions of 2021 so far, in reverse chronological order.
           </p>
-        </Link>
+          <Link
+            href="#contact"
+            className="inline-flex items-center px-3 py-2 text-sm
+          font-medium text-center text-white bg-blue-600 rounded-lg
+          hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300
+          dark:bg-blue-600 dark:hover:bg-blue-600 dark:focus:ring-blue-700"
+          >
+            Read more
+            <svg
+              aria-hidden="true"
+              className="w-4 h-4 ml-2 -mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </Link>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,12 +11,12 @@ const Skills = () => {
         <h1 className="text-4xl font-bold text-center text-[#f1f3f7]">
           Skills
         </h1>
-        <div className="container m-auto grid grid-cols-[30%_20%_30%_20%] gap-20 pt-20">
-          <h3 className="text-3xl text-justify font-medium text-white">
+        <div className="container m-auto grid grid-rows-1 sm:grid-cols-[30%_20%_30%_20%] sm:gap-20 pt-5 sm:pt-20">
+          <h3 className="text-3xl text-center sm:text-justify py-5 font-medium text-white">
             Languages:
           </h3>
           {/* <div className="flex flex-row justify-around"> */}
-          <div className="relative flex p-5 justify-start h-auto w-full shadow-xl shadow-gray-800 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#ebedf3]">
+          <div className="relative flex p-8 sm:text-left justify-center h-auto w-full shadow-xl shadow-gray-800 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#ebedf3]">
             <a
               href="https://www.w3schools.com/cpp/"
               target="_blank"
@@ -50,10 +50,10 @@ const Skills = () => {
               />
             </a>
           </div>
-          <h3 className="text-3xl text-justify font-medium text-white">
-            Database:
+          <h3 className="text-3xl text-center sm:text-justify py-5 font-medium text-white">
+            Databases
           </h3>
-          <div className="relative flex p-5 justify-start h-auto w-full shadow-xl shadow-gray-800 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#ebedf3]">
+          <div className="relative flex p-5 justify-center h-auto w-full shadow-xl shadow-gray-800 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#ebedf3]">
             <a
               href="https://www.postgresql.org"
               target="_blank"
@@ -88,10 +88,10 @@ const Skills = () => {
             </a>
           </div>
 
-          <h3 className="text-3xl text-justify font-medium text-white">
+          <h3 className="text-3xl text-center sm:text-justify py-5  font-medium text-white">
             Frontend Development:
           </h3>
-          <div className="relative flex p-5 justify-start h-auto w-full shadow-xl shadow-gray-800 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#ebedf3]">
+          <div className="relative flex p-5 justify-center h-auto w-full shadow-xl shadow-gray-800 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#ebedf3]">
             <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
               <Image
                 src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
@@ -138,10 +138,10 @@ const Skills = () => {
             </a>
           </div>
 
-          <h3 className="text-3xl text-justify font-medium text-white">
+          <h3 className="text-3xl text-center sm:text-justify py-5  font-medium text-white">
             Backend Development:
           </h3>
-          <div className="relative flex p-5 justify-start h-auto w-full shadow-xl shadow-gray-800 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#ebedf3]">
+          <div className="relative flex p-5 justify-center h-auto w-full shadow-xl shadow-gray-800 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#ebedf3]">
             <a
               href="https://flask.palletsprojects.com/"
               target="_blank"
@@ -180,10 +180,10 @@ const Skills = () => {
             </a>
           </div>
 
-          <h3 className="text-3xl text-justify font-medium text-white">
+          <h3 className="text-3xl text-center sm:text-justify py-5 font-medium text-white">
             Data Visualization:
           </h3>
-          <div className="relative flex p-5 justify-start h-auto w-full shadow-xl shadow-gray-800 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#ebedf3]">
+          <div className="relative flex p-5 justify-center h-auto w-full shadow-xl shadow-gray-800 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#ebedf3]">
             <a
               href="https://seaborn.pydata.org/"
               target="_blank"
@@ -206,7 +206,7 @@ const Skills = () => {
             </a>
           </div>
 
-          <h3 className="text-3xl text-justify font-medium text-white">
+          <h3 className="text-3xl text-center sm:text-justify py-5 font-medium text-white">
             AI/ML/CV:
           </h3>
           <div className="relative flex p-5 justify-start h-auto w-full shadow-xl shadow-gray-800 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#ebedf3]">
@@ -256,7 +256,7 @@ const Skills = () => {
             </a>
           </div>
 
-          <h3 className="text-3xl text-justify font-medium text-white">
+          <h3 className="text-3xl text-center sm:text-justify py-5 font-medium text-white">
             Tools:
           </h3>
           <div className="relative flex p-5 justify-start h-auto w-full shadow-xl shadow-gray-800 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#ebedf3]">
